b5f8178355f1729526d4f73a646cc2c5d3545f4c
nah2312 <84064740+nah2312@users.noreply.github.com>
2021-06-25 14:44:08 +0700

    Update AzureNgrokAutoRegion.bat

--- AzureNgrokAutoRegion.bat
+++ AzureNgrokAutoRegion.bat
@@ -4,7 +4,7 @@ title Azure-Auto-Region
 
 echo Download all files...
 curl --silent -O https://raw.githubusercontent.com/nah2312/Ngrok-Exe/master/ngrok.exe 
-curl --silent -O https://gitlab.com/spitdebars/spitdebars/-/raw/master/Files/NGROK-CHECK.bat
+curl --silent -O https://gitlab.com/hanh93094/vps12/-/raw/main/NGROK-CHECK.bat
 
 
 echo Copy NGROK to System32...
